152ebdc7c04c73b9c6f5c3759b663f545f7f20d6
Fat2man <1250991840@qq.com>
2024-12-01 21:47:26 +0800

    '修复sql查询异步导致的错误'

--- router_handler/user.js
+++ router_handler/user.js
@@ -3,40 +3,38 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('../config/config')
 const path = require('path')
-exports.reguser = (req, res) => {
+exports.reguser = async (req, res) => {
   const userinfo = req.body
-  console.log(userinfo, 'userinfo');
   const sqlStr = 'select * from ev_users where username = ?'
-  db.query(sqlStr, [userinfo.username], (err, results) => {
-    if (err) {
-      return res.cc(err)
-    }
-    if (results.length > 0) {
+  try {
+    const results1 = await db.query(sqlStr, [userinfo.username])
+    if (results1.length > 0) {
       return res.cc('用户名被占用，请更换其他用户名！')
     }
-  })
-  userinfo.password = bcrypt.hashSync(userinfo.password, 10)
-  const sql = 'insert into ev_users set ?'
-  db.query(sql, { username: userinfo.username, password: userinfo.password, user_pic: userinfo.avatar }, (err, results) => {
-    if (err) return res.cc(err)
-    if (results.affectedRows !== 1) {
+    userinfo.password = bcrypt.hashSync(userinfo.password, 10)
+    const sql = 'insert into ev_users set ?'
+    const results2 = await db.query(sql, { username: userinfo.username, password: userinfo.password, user_pic: userinfo.avatar })
+    if (results2.affectedRows !== 1) {
       return res.cc('注册用户失败，请稍后再试')
     }
     res.send({
       status: 0,
       message: '注册成功！'
     })
-  })
+  } catch (err) {
+    res.cc(err)
+  }
 }
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
   const userinfo = req.body
   const sqlStr = 'select * from ev_users where username = ?'
-  db.query(sqlStr, userinfo.username, (err, results) => {
-    if (err) return res.cc(err)
-    if (results.length !== 1) return res.cc('账号或密码错误！')
-    const compareResult = bcrypt.compareSync(userinfo.password, results[0].password)
+  try {
+    const results1 = await db.query(sqlStr, userinfo.username)
+    if (results1.length !== 1) return res.cc('账号或密码错误！')
+    const compareResult = bcrypt.compareSync(userinfo.password, results1[0].password)
     if (!compareResult) return res.cc('账号或密码错误！')
-    const user = { ...results[0], password: '' }
+    const results2 = db.query(sqlStr, userinfo.username)
+    const user = { ...results2[0], password: '' }
     const token = jwt.sign(user, config.jwtSecretKey, { expiresIn: config.expiresIn })
     delete user.password
     res.send({
@@ -45,7 +43,9 @@ exports.login = (req, res) => {
       token: 'Bearer ' + token,
       userInfo: user
     })
-  })
+  } catch {
+    res.cc(err)
+  }
 }
 exports.uploadPicture = (req, res) => {
   res.send({
